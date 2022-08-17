84e1e6b7bc6518a84b6a710a190f700e4aec86ac
jesse-marks <18119820+jesse-marks@users.noreply.github.com>
2022-08-17 12:00:26 -0400

    add initial C#/.NET code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using NBitcoin;
+
+namespace Jesse
+{
+    class KeysAddresses
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1; i++ )
+            {
+                Key privateKey = new Key();
+                PubKey publicKey = privateKey.PubKey;
+                Console.WriteLine("P2PKH: " + publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main));
+                Console.WriteLine("Taproot: " + publicKey.GetAddress(ScriptPubKeyType.TaprootBIP86, Network.Main));
+                Console.WriteLine("P2WPKH: " + publicKey.GetAddress(ScriptPubKeyType.Segwit, Network.Main));
+                Console.WriteLine("P2WPKH-P2SH: " + publicKey.GetAddress(ScriptPubKeyType.SegwitP2SH, Network.Main));
+                Console.WriteLine("");
+            }
+        }
+    }
+}
